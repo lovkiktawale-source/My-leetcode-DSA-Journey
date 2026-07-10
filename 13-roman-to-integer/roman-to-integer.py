cd3ea9135e9427c2cc2e32daa2cd73313328471e
Lovkik Tawale <lovkiktawale@gmail.com>
2026-07-10 23:31:18 +0530

    Time: 8 ms (65.66%) | Memory: 12.3 MB (56.48%) - LeetSync

---
+++ 13-roman-to-integer/roman-to-integer.py
@@ -0,0 +1,29 @@
+class Solution(object):
+    def romanToInt(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        # Map each Roman numeral character to its integer value
+        roman_map = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        }
+        
+        total = 0
+        n = len(s)
+        
+        for i in range(n):
+            # If the current value is less than the next value, subtract it
+            if i < n - 1 and roman_map[s[i]] < roman_map[s[i + 1]]:
+                total -= roman_map[s[i]]
+            else:
+                # Otherwise, add it
+                total += roman_map[s[i]]
+                
+        return total
